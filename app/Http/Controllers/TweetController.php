cd83e491b702a1e09bd6c04858b0e3e639a90f13
Yurii Rybachok <drummer2009@ukr.net>
2023-04-06 21:24:44 +0300

    create store & delete methods

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -23,38 +23,50 @@ class TweetController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
+        $file = null;
+        $extension = null;
+        $fileName = null;
+        $path = '';
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Tweet $tweet)
-    {
-        //
-    }
+        if ($request->hasFile('file')) {
+            $file = $request->file('file');
+            $request->validate([ 'file' => 'required|mimes:jpg,jpeg,png,mp4' ]);
+            $extension = $file->getClientOriginalExtension();
+            $fileName = time() . '.' . $extension;
+            $extension === 'mp4' ? $path = '/videos/' : $path = '/pics/';
+        }
+        $tweet = new Tweet;
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Tweet $tweet)
-    {
-        //
-    }
+        $tweet->name = 'Urii Rybachok Dev';
+        $tweet->handle = '@uriirybachokdev';
+        $tweet->image = 'https://avatars.githubusercontent.com/u/104525685?v=4';
+        $tweet->tweet = $request->input('tweet');
+        if ($fileName) {
+            $tweet->file = $path . $fileName;
+            $tweet->is_video = $extension === 'mp4' ? true : false;
+            $file->move(public_path() . $path, $fileName);
+        }
+        $tweet->comments = rand(5, 500);
+        $tweet->retweets = rand(5, 500);
+        $tweet->likes = rand(5, 500);
+        $tweet->analytics = rand(5, 500);
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Tweet $tweet)
-    {
-        //
+        $tweet->save();
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Tweet $tweet)
+    public function destroy($id)
     {
-        //
+        $tweet = Tweet::find($id);
+
+        if (!is_null($tweet->file) && file_exists(public_path() . $tweet->file)) {
+            unlink(public_path() . $tweet->file);
+        }
+
+        $tweet->delete();
+
+        return redirect()->route('tweets.index');
     }
 }
