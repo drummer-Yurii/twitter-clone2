9535b2a7b054f92a7adde05a49afd1c6b5439e06
Yurii Rybachok <drummer2009@ukr.net>
2023-04-06 21:48:25 +0300

    create the addTweet function

--- resources/js/Layouts/TwitterLayout.vue
+++ resources/js/Layouts/TwitterLayout.vue
@@ -41,6 +41,19 @@ const textareaInput = (e) => {
     textarea.value.style.height = `${e.target.scrollHeight}px`;
 }
 
+const addTweet = () => {
+    if (!tweet.value) return
+
+    let data = new FormData()
+
+    data.append('tweet', tweet.value)
+    data.append('file', file.value)
+
+    router.post('/tweets', data)
+
+    closeMessageBox()
+}
+
 </script>
 
 <template>
@@ -231,6 +244,7 @@ const textareaInput = (e) => {
                 </div>
 
                 <button
+                    @click="addTweet()"
                     :desabled="!tweet"
                     :class="tweet ? 'bg-[#1C9CEF] text-white' : 'bg-[#124D77] text-gray-400'"
                     class="md:hidden font-extrabold text-[16px] p-1.5 px-4 rounded-full cursor-pointer"
@@ -296,6 +310,7 @@ const textareaInput = (e) => {
                             </div>
                         </div>
                         <button
+                            @click="addTweet()"
                             :desabled="!tweet"
                             :class="tweet ? 'bg-[#1C9CEF] text-white' : 'bg-[#124D77] text-gray-400'"
                             class="hidden md:block font-extrabold text-[16px] p-1.5 px-4 rounded-full cursor-pointer"
