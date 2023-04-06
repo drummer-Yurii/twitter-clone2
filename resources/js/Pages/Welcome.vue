98e0686f9aa871b382a90cba8ce5cde73053134e
Yurii Rybachok <drummer2009@ukr.net>
2023-04-06 20:25:49 +0300

    display tweets section

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { Head, Link } from '@inertiajs/vue3';
+import Tweet from '@/Components/Tweet.vue';
 import TwitterLayout from '@/Layouts/TwitterLayout.vue';
 
 </script>
@@ -8,6 +9,22 @@ import TwitterLayout from '@/Layouts/TwitterLayout.vue';
     <Head title="Welcome" />
 
     <TwitterLayout>
-        <!-- <div class="bg-red-900">Welcome</div> -->
+        <div class="text-white">
+            <div class="flex">
+                <Tweet :tweet="{
+                    name: 'Johnni Ward',
+                    handle: '@johnniward',
+                    image: 'https://randomuser.me/api/portraits/men/40.jpg',
+                    tweet: 'We went rock climbing this weekend? Here is the video. Climbing is way more fun than exercising on any gym equipment. It works both your mind and body',
+                    file: '/videos/Sportsman.mp4',
+                    is_video: true,
+                    comments: '35',
+                    retweets: '54',
+                    likes: '88',
+                    analytics: '81',
+                }" />
+            </div>
+            <div class="border-b border-b-gray-800 mt-2"></div>
+        </div>
     </TwitterLayout>
 </template>
